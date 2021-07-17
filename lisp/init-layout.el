09551d20d0a7a62f1cbc8bc3a973da86fd8d48ed
SirObi <obi.orciuch@gmail.com>
2021-07-17 09:14:07 +0100

    Use Atom One Dark theme

--- lisp/init-layout.el
+++ lisp/init-layout.el
@@ -1,6 +1,6 @@
 ;; define list of packages to install
 (defvar emacsLayoutPackages
-  '(material-theme))
+  '(atom-one-dark-theme))
 
 ;; install all packages in list
 (mapc #'(lambda (package)
@@ -8,19 +8,27 @@
       (package-install package)))
       emacsLayoutPackages)
 
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))                       ;; hide toolbar
+(load-theme 'atom-one-dark t)                 ;; load Atom theme
 
-(load-theme 'material t)                    ;; load material theme
-(menu-bar-mode -1)                          ;; hide menu
-(global-linum-mode t)                       ;; enable line numbers globally
-(setq linum-format "%4d \u2502 ")           ;; format line number spacing
-(setq inhibit-startup-message t)            ;; hide the startup message
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))                         ;; hide toolbar
+(menu-bar-mode -1)                            ;; hide menu
+(setq inhibit-startup-message t)              ;; hide the startup message
+
+(set-face-attribute 'default nil :height 120) ;; Set font size to 12
+                                              ;; Uncomment the line below to try an Asian-style font
+;;(set-face-attribute 'default nil :font "-GOOG-Noto Sans Mono CJK SC-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1")
+
+
 (setq-default
- blink-cursor-interval 0.4)                 ;; nicer cursor blink interval
+ blink-cursor-interval 0.4)                   ;; nicer cursor blink interval
 (when (maybe-require-package 'beacon)
   (setq-default beacon-lighter "")
   (setq-default beacon-size 20)
-  (add-hook 'after-init-hook 'beacon-mode)) ;; little flash of light to help you keep track of the cursor
+  (add-hook 'after-init-hook 'beacon-mode))   ;; little flash of light to help you keep track of the cursor
+
+
+(global-linum-mode t)                         ;; enable line numbers globally
+(setq linum-format "%4d \u2502 ")             ;; format line number spacing
 
 (provide 'init-layout)
