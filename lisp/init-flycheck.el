686a0e4323b777643744168c9755ce0a993a047a
SirObi <obi.orciuch@gmail.com>
2021-08-19 14:58:25 +0100

    Allow flake8 and mypy to use pyproject.toml as config file

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,6 +6,10 @@
   (add-hook 'after-init-hook 'global-flycheck-mode)
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
+  ;; Enable flake8 and mypy to use pyproject.toml as their config file
+  (setq flycheck-python-mypy-config '("mypy.ini" "setup.cfg" "pyproject.toml"))
+  (setq flycheck-flake8rc '(".flake8rc" "pyproject.toml"))
+
   (when (maybe-require-package 'flycheck-color-mode-line)
     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
